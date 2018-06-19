c3c7f2c8f9c4bc1eb0f0921ad6177a016c2ce50d
David Cernat <davidcernat@gmail.com>
2018-06-20 02:00:12 +0300

    [Server] Don't automatically synchronize ObjectDelete packets

--- apps/openmw-mp/processors/object/ProcessorObjectDelete.hpp
+++ apps/openmw-mp/processors/object/ProcessorObjectDelete.hpp
@@ -16,8 +16,6 @@ namespace mwmp
         void Do(ObjectPacket &packet, Player &player, BaseObjectList &objectList) override
         {
             LOG_MESSAGE_SIMPLE(Log::LOG_INFO, "Received %s from %s", strPacketID.c_str(), player.npc.mName.c_str());
-            
-            packet.Send(true);
 
             Script::Call<Script::CallbackIdentity("OnObjectDelete")>(player.getId(), objectList.cell.getDescription().c_str());
         }
