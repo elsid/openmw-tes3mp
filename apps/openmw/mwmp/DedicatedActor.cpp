OpenMW/openmw

commit 2427a4f877d149e30d9e94adcca38268ebc3fc92

[Client] Update DedicatedActor to C++11

apps/openmw/mwmp/DedicatedActor.cpp
@@ -178,9 +178,7 @@ void DedicatedActor::setEquipment()
             storeRefId = it->getCellRef().getRefId();
 
             if (!Misc::StringUtils::ciEqual(storeRefId, packetRefId)) // if other item equiped
-            {
                 invStore.unequipSlot(slot, ptr);
-            }
             else
                 equal = true;
         }
@@ -227,14 +225,10 @@ void DedicatedActor::playSound()
 
 bool DedicatedActor::hasItem(std::string refId, int charge)
 {
-    MWWorld::InventoryStore& invStore = ptr.getClass().getInventoryStore(ptr);
-
-    for (MWWorld::ContainerStoreIterator it = invStore.begin(); it != invStore.end(); ++it)
+    for (const auto &ptr : ptr.getClass().getInventoryStore(ptr))
     {
-        if (::Misc::StringUtils::ciEqual(it->getCellRef().getRefId(), refId) && it->getCellRef().getCharge() == charge)
-        {
+        if (::Misc::StringUtils::ciEqual(ptr.getCellRef().getRefId(), refId) && ptr.getCellRef().getCharge() == charge)
             return true;
-        }
     }
 
     return false;
@@ -242,14 +236,12 @@ bool DedicatedActor::hasItem(std::string refId, int charge)
 
 void DedicatedActor::equipItem(std::string refId, int charge)
 {
-    MWWorld::InventoryStore& invStore = ptr.getClass().getInventoryStore(ptr);
-
-    for (MWWorld::ContainerStoreIterator it = invStore.begin(); it != invStore.end(); ++it)
+    for (const auto &ptr : ptr.getClass().getInventoryStore(ptr))
     {
-        if (::Misc::StringUtils::ciEqual(it->getCellRef().getRefId(), refId) && it->getCellRef().getCharge() == charge)
+        if (::Misc::StringUtils::ciEqual(ptr.getCellRef().getRefId(), refId) && ptr.getCellRef().getCharge() == charge)
         {
-            std::shared_ptr<MWWorld::Action> action = it->getClass().use(*it);
-            action->execute(ptr);
+            std::shared_ptr<MWWorld::Action> action = ptr.getClass().use(ptr);
+            action->execute(this->ptr);
             break;
         }
     }

elsid/openmw-tes3mp

commit f1315ef30d5b86d7a88b37d0996dd48c88e288fa

[Cllient] Differentiate itemPtr from actor Ptr in DedicatedActor methods
2427a4f877d149e30d9e94adcca38268ebc3fc92 mistakenly used the variable name "ptr" for both the actor and the items in that actor's inventory.

apps/openmw/mwmp/DedicatedActor.cpp
@@ -341,9 +341,9 @@ void DedicatedActor::playSound()
 
 bool DedicatedActor::hasItem(std::string refId, int charge)
 {
-    for (const auto &ptr : ptr.getClass().getInventoryStore(ptr))
+    for (const auto &itemPtr : ptr.getClass().getInventoryStore(ptr))
     {
-        if (::Misc::StringUtils::ciEqual(ptr.getCellRef().getRefId(), refId) && ptr.getCellRef().getCharge() == charge)
+        if (::Misc::StringUtils::ciEqual(itemPtr.getCellRef().getRefId(), refId) && itemPtr.getCellRef().getCharge() == charge)
             return true;
     }
 
@@ -352,12 +352,12 @@ bool DedicatedActor::hasItem(std::string refId, int charge)
 
 void DedicatedActor::equipItem(std::string refId, int charge)
 {
-    for (const auto &ptr : ptr.getClass().getInventoryStore(ptr))
+    for (const auto &itemPtr : ptr.getClass().getInventoryStore(ptr))
     {
-        if (::Misc::StringUtils::ciEqual(ptr.getCellRef().getRefId(), refId) && ptr.getCellRef().getCharge() == charge)
+        if (::Misc::StringUtils::ciEqual(itemPtr.getCellRef().getRefId(), refId) && itemPtr.getCellRef().getCharge() == charge)
         {
-            std::shared_ptr<MWWorld::Action> action = ptr.getClass().use(ptr);
-            action->execute(this->ptr);
+            std::shared_ptr<MWWorld::Action> action = itemPtr.getClass().use(itemPtr);
+            action->execute(ptr);
             break;
         }
     }