7edfca06cfc22855b78e86e4f23a28e1f776bd9e
David Cernat <davidcernat@gmail.com>
2019-08-20 13:15:57 +0300

    [CI] Fix issues in before_install.linux.sh

--- CI/before_install.linux.sh
+++ CI/before_install.linux.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
-sudo ln -s /usr/bin/clang-3.6 /usr/local/bin/clang
-sudo ln -s /usr/bin/clang++-3.6 /usr/local/bin/clang++
-
 echo -n | openssl s_client -connect scan.coverity.com:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sudo tee -a /etc/ssl/certs/ca-
 
 # Set up compilers
 if [ ! -z "${MATRIX_CC}" ]; then
     eval "${MATRIX_CC}"
 fi
-
-# build libgtest & libgtest_main
-sudo mkdir /usr/src/gtest/build
-cd /usr/src/gtest/build
-sudo cmake .. -DBUILD_SHARED_LIBS=1
-sudo make -j4
-sudo ln -s /usr/src/gtest/build/libgtest.so /usr/lib/libgtest.so
-sudo ln -s /usr/src/gtest/build/libgtest_main.so /usr/lib/libgtest_main.so
 
 cd ~/
 git clone https://github.com/TES3MP/CrabNet
